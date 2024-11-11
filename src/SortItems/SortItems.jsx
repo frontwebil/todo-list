18379b823e136ec3e4fab0fe527b35d6ae879abb
iLyhaha <gollpfd@gmail.com>
2024-11-12 01:15:16 +0200

    fixed

--- src/SortItems/SortItems.jsx
+++ src/SortItems/SortItems.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import './SortItems.css';
 
 export function SortItems({sortByPriorityDescending , sortByPriorityAscending , setDefault}) {
+
+  const [activeButton , setActiveButton] = useState('default')
+
+  function handleSortDescending(){
+    setActiveButton('descending')
+    sortByPriorityDescending()
+  }
+  function handleSortAscending(){
+    setActiveButton('ascending')
+    sortByPriorityAscending()
+  }
+  function handleSortDefault(){
+    setActiveButton('default')
+    setDefault()
+  }
+
   return (
     <div className="Sort-buttons">
-      <button className="sort-btn active" onClick={sortByPriorityDescending}>Sort by priority in descending</button>
-      <button className="sort-btn active" onClick={sortByPriorityAscending}>Sort by priority in ascending</button>
-      <button className='sort-btn active' onClick={setDefault}>Default</button>
+      <button className={`sort-btn ${activeButton === 'descending' ? 'active' : ''}`}  onClick={handleSortDescending}>Sort by priority in descending</button>
+      <button className={`sort-btn ${activeButton === 'ascending' ? 'active' : ''}`}  onClick={handleSortAscending}>Sort by priority in ascending</button>
+      <button className={`sort-btn ${activeButton === 'default' ? 'active' : ''}`}  onClick={handleSortAscending}>Default</button>
     </div>
   )
 }
